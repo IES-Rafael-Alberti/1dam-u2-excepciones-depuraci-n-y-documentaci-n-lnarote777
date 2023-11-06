96b1cf5263df0a7aa4599d603f11036fbcb3f479
lnarote777 <nolara@iesrafaelalberti.es>
2023-11-06 21:34:08 +0100

    practica 2.3

---
+++ src/act2_3_1.py
@@ -0,0 +1,31 @@
+"""
+Ejercicio 2.3.1
+
+Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+"""
+
+def cumplido(edad):
+    if edad < 0 :
+        raise ValueError ("La edad no puede ser negativa.")
+    
+    cont = 1
+    years = str()
+    for i in range(1, edad +1):
+        cont += i
+        if i != edad:
+            years = str(f"{i}, ")
+        else:
+            years = str(f"{edad} años. ")
+    return years   
+    
+def main(): 
+    edad = int(input("Introduzca su edad: "))
+    try: 
+        print(cumplido(edad))
+    except ValueError as e:
+        print("***ERROR*** - " + str(e))
+        
+    
+    
+if __name__ == "__main__":
+    main()
