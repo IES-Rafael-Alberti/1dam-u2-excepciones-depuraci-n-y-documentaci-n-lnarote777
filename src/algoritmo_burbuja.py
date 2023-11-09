9ee9e85e3f7363391e71f93ce79e53401df32415
lnarote777 <nolara@iesrafaelalberti.es>
2023-11-09 21:23:11 +0100

    algoritmo_burbuja

--- src/algoritmo_burbuja.py
+++ src/algoritmo_burbuja.py
@@ -1,17 +1,20 @@
 
-def ordenarLista():
-    a = [8, 3, 1, 19, 14]
+def ordenarLista(a):
     total =len(a)
+    cont = 0
     for i in range(0, len(a)):
         for j in range(0, total - 1):
             if a[j] > a[j+1]:
                 a[j] , a[j+1] = a[j+1] , a[j]
-    			
+                cont +=1
+        total -=1
+    return a		
          
     
     
 def main():
-	print(ordenarLista())
+	a = [8, 3, 1, 19, 14]
+	print(ordenarLista(a))
 
 
 if __name__ == "__main__":
